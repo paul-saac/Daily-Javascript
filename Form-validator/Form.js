bf3415c8b83d8801c1ad84b6ff1aced623bed463
paul-saac <johnpaul.saac.legaspi@gmail.com>
2025-06-24 00:20:57 +0800

    dasdasdasd

--- Form-validator/Form.js
+++ Form-validator/Form.js
@@ -2,11 +2,12 @@ const form = document.getElementById("registerForm");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
+const password = document.getElementById("asd");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault(); // Stop form from refreshing the page
 
-  
+
   if (username.value === "" || email.value === "") {
     message.textContent = "Please fill in all fields.";
     message.style.color = "orange";
